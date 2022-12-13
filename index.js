d62fbc675fc677f762253e2cfc732640f3fbff5e
nishat <nishatroyhana011@gmail.com>
2022-12-13 15:04:47 +0600

    hospital details api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,6 +15,7 @@ async function run(){
     try{
         const hospitalCollection = client.db("patient-register").collection("hospitals");
         const psychiatristCollection = client.db("patient-register").collection("psychiatrists");
+        const patientCollection = client.db("patient-register").collection("patients");
 
         app.get('/hospitals', async(req, res)=>{
             const query = {}
@@ -26,6 +27,37 @@ async function run(){
             const Psychiatrist = req.body;
             const result = await psychiatristCollection.insertOne(Psychiatrist);
             res.send(result)
+        });
+
+        app.get('/Psychiatrist', async (req, res)=>{
+            const email = req.query.email;
+            const query = {email:email};
+            const result = await psychiatristCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.post('/patient', async(req, res)=>{
+            const patient = req.body;
+            const result = await patientCollection.insertOne(patient);
+            res.send(result)
+        });
+
+        app.get('/hospital/:id', async(req, res)=>{
+            const id = req.params.id;
+            const queryId = {_id: ObjectId(id)}
+            const hospitalName = await hospitalCollection.findOne(queryId);
+            const query = { hospital: id }
+            const doctorsArray = await psychiatristCollection.find(query).toArray();
+            const patientArray = await patientCollection.find(query).toArray();
+            res.send({hospitalName, doctorsArray, patientArray})
+        });
+
+        app.get('/patient', async(req, res)=>{
+            const id = req.query.psychiatristId;
+            const query = {psychiatristId:id};
+            const patient =  await patientCollection.count(query);
+            const pcount = [patient]
+            res.send(pcount)
         })
         
     }catch{
